0ccb484df3b3fc1266553e606ec2547712242d77
Taeyoon Kim <tykimseoul@gmail.com>
2019-07-10 00:34:48 +0900

    add: receive touch points in control view

--- app/src/main/java/com/example/pc/oreo/ControlView.kt
+++ app/src/main/java/com/example/pc/oreo/ControlView.kt
@@ -1,18 +1,116 @@
 package com.example.pc.oreo
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.util.DisplayMetrics
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import com.curiouscreature.kotlin.math.Float2
 
-class ControlView: View {
-    lateinit var screenSize:Float2
+class ControlView : View {
+    private val screenSize: Float2 by lazy {
+        val displayMetrics = DisplayMetrics()
+        (context as MainActivity).windowManager.defaultDisplay.getMetrics(displayMetrics)
+        Float2(displayMetrics.widthPixels.toFloat(), displayMetrics.heightPixels.toFloat())
+    }
+    val controlViewListener: ControlViewListener by lazy { context as MainActivity }
+    lateinit var ids: Array<Int?>
+    lateinit var types: Array<TouchType?>
+
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    fun consumeTouch(touchPoint:Float2):Boolean{
+    private fun consumeTouch(pair: Pair<Float2?, TouchType?>) {
+        pair.first?.let {
+            when (pair.second) {
+                TouchType.STEER -> calculateSteerAngle(it).let { angle ->
+                    controlViewListener.onSteer(angle)
+                }
+                TouchType.ACCELERATE -> calculateAcceleration(it).let { speed ->
+                    controlViewListener.onAccelerate(speed)
+                }
+            }
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                ids = arrayOf(event.getPointerIdOrElse(0), event.getPointerIdOrElse(1))
+                types = ids.map { i -> getTouchType(event.getPointerCoordinates(i)) }.toTypedArray()
+            }
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                ids[event.actionIndex] = event.getPointerId(event.actionIndex)
+                types = ids.map { i -> getTouchType(event.getPointerCoordinates(i)) }.toTypedArray()
+                if (types.size != types.distinct().size) {
+                    ids[event.actionIndex] = null
+                    types[event.actionIndex] = null
+                }
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+                ids[event.actionIndex] = null
+                types = ids.map { i -> getTouchType(event.getPointerCoordinates(i)) }.toTypedArray()
+            }
+            MotionEvent.ACTION_UP -> {
+                ids.fill(null)
+                types.fill(null)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val points = ids.map { i -> event.getPointerCoordinates(i) }
+                (points zip types).forEach { consumeTouch(it) }
+                performClick()
+            }
+        }
+        Log.e("touch", "[${ids.joinToString()}], [${types.joinToString()}]")
         return true
+    }
+
+    fun MotionEvent.getPointerIdOrElse(idx: Int): Int? {
+        return try {
+            getPointerId(idx)
+        } catch (e: IllegalArgumentException) {
+            null
+        }
+    }
+
+    fun MotionEvent.getPointerCoordinates(id: Int?): Float2? {
+        return id?.let {
+            findPointerIndex(it).let { pointerIndex ->
+                Float2(getX(pointerIndex), getY(pointerIndex))
+            }
+        }
+    }
+
+    private fun getTouchType(touchPoint: Float2?): TouchType? {
+        return touchPoint?.let {
+            if (it.x < screenSize.x / 2) {
+                TouchType.STEER
+            } else {
+                TouchType.ACCELERATE
+            }
+        }
+    }
+
+    private fun calculateSteerAngle(touchPoint: Float2): Int {
+        val xDisplacement = touchPoint.x - screenSize.x / 4.0
+        return 0
+    }
+
+    private fun calculateAcceleration(touchPoint: Float2): Int {
+        return 0
+    }
+
+    enum class TouchType constructor(val value: Int) {
+        STEER(1),
+        ACCELERATE(2)
+    }
+
+    interface ControlViewListener {
+        fun onSteer(angle: Int)
+        fun onAccelerate(power: Int)
     }
 }
